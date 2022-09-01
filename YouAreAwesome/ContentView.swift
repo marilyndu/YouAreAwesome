4c7d2354ef0bce04a89b431ed443f99ab43ba206
Marilyn Du <dumf@bc.edu>
2022-09-01 18:18:32 -0400

    Initial Commit

---
+++ YouAreAwesome/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  YouAreAwesome
+//
+//  Created by Marilyn Du on 9/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
